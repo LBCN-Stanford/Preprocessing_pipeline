6e3f1c871b7ffa999ccbc85fd866292fd3983462
Jessica Schrouff <jschrouff@ulg.ac.be>
2017-07-05 13:08:15 +0100

    Tutorial update for event related

--- utils/script_ALLPreprocessing_MMR_example.m
+++ utils/script_ALLPreprocessing_MMR_example.m
@@ -1,53 +1,47 @@
-function script_Event_Preprocessing_example(filename,sodata,bch)
+function script_ALLPreprocessing_MMR_example(filename,sodata,bch)
 
 % Function to perform full pre-processing and univariate analysis for a
 % specific block of edf data.
 % Inputs:
-% - filename: name of data file in edf format (optional)
-% - sodata  : name of SODATA .mat (optional)
-% - bch     : indexes or names (cell array) of pathological channels to
-%             exclude.
+% - filename: name of data file in SPM format (optional)
+% - sodata  : name of behavioral data .mat (optional)
+% - bch     : name of bad channel file (optional)
 % -------------------------------------------------------------------------
 
 % Get inputs
 if nargin<1 || isempty(filename)
-    filename = spm_select(inf,'any','Select file to convert',{},pwd,'.mat');
+    filename = spm_select(1,'mat','Select file to convert',{},pwd,'.mat');
 end
 
 if nargin<2 || isempty(sodata)
-    sodata = spm_select(inf,'mat','Select event file',{},pwd,'.mat');
+    sodata = spm_select(1,'mat','Select behavioral data for file',{},pwd,'.mat');
 end
 
 if nargin<3 || isempty(bch)
+    bch = spm_select(1,'mat','Select bad channels info for file',{},pwd,'.mat');
+end
+try
+    load(bch)
+catch
     bch = [];
 end
 
 
-RTbased = 0;
-if RTbased
-    fieldepoch = 'start';
-    twepoch = [-300 2200]; %Max of RTs as default
-    bc = 1;
-    bcfield = 'start';
-    twbc = [-150 0];
-    twResc = [-150 0];
-    smoothwin = 100;
-    twsmooth = [-150 2000];
-    RTbased = 1;
-    twsmoothRT = [-2000 150];
-    fieldepochRT = 'RTons';
-    twepochRT = [-2200 300];
-else
-    fieldepoch = 'start';
-    twepoch = [-200 1200];
-    bc = 1;
-    bcfield = 'start';
-    twbc = [-200 0];
-    twResc = [-100 0];
-    smoothwin = 100;
-    twsmooth = [-100 1000];
-end
-
+%Set epoching, smoothing and RT rescaling parameters
+fieldepoch = 'start';
+twepoch = [-300 2200]; %Max of RTs as default
+bc = 1;
+bcfield = 'start';
+twbc = [-150 0];
+twResc = [-150 0];
+smoothwin = 100;
+twsmooth = [-150 2000];
+RTbased = 1;
+twsmoothRT = [-2000 150];
+fieldepochRT = 'RTons';
+twepochRT = [-2200 300];
+task = 'MMR';
+    
 
 % Filter data and detect bad channels
 fd = LBCN_filter_badchans(filename,[], bch,1,0.5);
@@ -62,7 +56,7 @@ for i = 1: numel(fd)
     fname = fullfile(d{1}.path,d{1}.fname);
     
     % Epoch data using event file
-    D = LBCN_epoch_bc(fname,deblank(sodata(i,:)),[],fieldepoch,twepoch,bc,bcfield,twbc); 
+    D = LBCN_epoch_bc(fname,sodata,[],fieldepoch,twepoch,bc,bcfield,twbc); 
     fnameons = fullfile(D.path,D.fname);
     
     % Perform artifact rejection, TF
@@ -76,7 +70,7 @@ for i = 1: numel(fd)
     
     if RTbased
         % Epoch based on RT, correct based on onset
-        D = LBCN_epoch_bc(fname,evtfile{i},[],fieldepochRT,twepochRT,bc,bcfield,twbc,'RT');
+        D = LBCN_epoch_bc(fname,sodata,[],fieldepochRT,twepochRT,bc,bcfield,twbc,'RT');
         fnameons = fullfile(D.path,D.fname);
         
         % Perform artifact rejection, TF
@@ -108,15 +102,17 @@ if RTbased
         S.recode = 'same';
         D = spm_eeg_merge(S);
         finalRT = fullfile(D.path,D.fname);
+    else
+        finalRT = tomerge;
     end
     % Plot the HFB for all conditions
-    LBCN_plot_averaged_signal_epochsRT(final,finalRT);
+    LBCN_plot_averaged_signal_epochsRT(final,finalRT,[],[],[],[], 1,task);
 else
     % Plot the HFB for all conditions
-    LBCN_plot_averaged_signal_epochs(final);
+    LBCN_plot_averaged_signal_epochs(final,[],[],[], 1,task);
 end
 
-
+% Perform simple univariate analysis: each condition versus baseline
 
 
 
